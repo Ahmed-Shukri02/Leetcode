ddff8b7ed5c7243bedad4526a82ce71f2c43d05f
Ahmed Shukri <79210672+Ahmed-Shukri02@users.noreply.github.com>
2022-09-10 11:20:48 +0100

    Time: 400 ms (14.24%), Space: 15 MB (8.94%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -7,13 +7,13 @@ class Solution(object):
         """
         
         l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = (l + r)// 2
-            if target < nums[mid]:
-                r = mid - 1
-            elif target > nums[mid]:
+        while l<=r:
+            mid = (l+r)//2
+            if target > nums[mid]: # remove left side
                 l = mid + 1
+            elif target < nums[mid]:
+                r = mid - 1
             else:
                 return mid
-        
         return -1
+                
