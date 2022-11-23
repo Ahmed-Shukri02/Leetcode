b8efbfca53f666360fe68010e0e3b03c274156b0
Ahmed Shukri <79210672+Ahmed-Shukri02@users.noreply.github.com>
2022-11-23 16:08:27 +0000

    Time: 204 ms (25.63%), Space: 41.7 MB (47.39%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cs
+++ 0036-valid-sudoku/0036-valid-sudoku.cs
@@ -1,46 +1,35 @@
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        quad = {(i, j): set() for i in range(3) for j in range(3)}
-        row, col = [set() for i in range(9)] , [set() for i in range(9)]
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        Dictionary<(int, int), HashSet<int>> quad = new Dictionary<(int, int), HashSet<int>>();
+        List<HashSet<int>> row, col;
         
-        ROWS, COLS = len(board), len(board[0])
-        for i in range(ROWS):
-            for j in range(COLS):
-                num, q = board[i][j], (i//3, j//3)
-                if num == ".": continue
-                if num in quad[q] or num in row[i] or num in col[j]:
-                    
-                    return False
-                quad[q].add(num)
-                row[i].add(num)
-                col[j].add(num)
-        return True
+        row = new List<HashSet<int>>();
+        col = new List<HashSet<int>>();
         
-        def dfs(i, j):
-            if (i >= ROWS):
-                return True
+        for(int i = 0; i < 9; i++){
+            row.Add(new HashSet<int>());
+            col.Add(new HashSet<int>());
             
-            if board[i][j] != ".":
-                newi, newj = i, j + 1 if j + 1 < COLS else i + 1, 0
-                return dfs(newi, newj)
-            
-            q = (i // 3, j // 3)
-            valid = False
-            for num in range(10):
-                if num not in quad[q] and num not in row[i] and num not in col[j]:
-                    quad[q].add(num)
-                    row[i].add(num)
-                    col[j].add(num)
-                    
-                    newi, newj = i, j + 1 if j + 1 < COLS else i + 1, 0
-                    if dfs(newi, newj):
-                        valid = True
-                    
-                    quad[q].remove(num)
-                    row[i].remove(num)
-                    col[j].remove(num)
-                    
-                    if valid:
-                        break
-            return valid
-                    
+            for(int j = 0; j < 9; j ++){
+                quad.Add((i, j), new HashSet<int>());
+            }
+        }
+        
+        for(int i=0; i < 9; i++){
+            for(int j=0; j< 9; j++){
+                char num = board[i][j];
+                if(num == '.') { continue; }
+                (int, int) q = ((int) Math.Floor((double)(i / 3)), (int) Math.Floor((double)(j / 3)));
+                if(quad[q].Contains(num) || row[i].Contains(num) || col[j].Contains(num)){
+                    return false;
+                }
+                quad[q].Add(num);
+                row[i].Add(num);
+                col[j].Add(num);
+            }
+        }
+        
+        return true;
+        
+    }
+}
