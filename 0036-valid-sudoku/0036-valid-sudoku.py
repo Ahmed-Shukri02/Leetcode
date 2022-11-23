411c75ee26aaebce2232b369d28f3e3cb52a383d
Ahmed Shukri <79210672+Ahmed-Shukri02@users.noreply.github.com>
2022-11-23 15:50:45 +0000

    Time: 248 ms (28.52%), Space: 14.1 MB (6.88%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,42 +1,46 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        quad = {(i, j): set() for i in range(3) for j in range(3)}
+        row, col = [set() for i in range(9)] , [set() for i in range(9)]
         
-        # O(n^2) time with O(n) space
-        
-        # create 19 hash maps
-        cols = [{} for i in range(9)]
-        
-        squareMaps = {(0, 0): {}, (0, 1): {}, (0, 2): {},
-                      (1, 0): {}, (1, 1): {}, (1, 2): {},
-                      (2, 0): {}, (2, 1): {}, (2, 2): {}}
-        
-        
-        for i, row in enumerate(board):
-            row_hmap = {}
-            for j, item in enumerate(row):
-                # is item .?
-                if item == ".":
-                    continue
-                  
-                # is item in row hmap?
-                if row_hmap.get(item, None):
+        ROWS, COLS = len(board), len(board[0])
+        for i in range(ROWS):
+            for j in range(COLS):
+                num, q = board[i][j], (i//3, j//3)
+                if num == ".": continue
+                if num in quad[q] or num in row[i] or num in col[j]:
+                    
                     return False
-                # is item in corresponding col?
-                elif cols[j].get(item, None):
-                    return False
-                
-                squareMap = squareMaps[(i // 3, j // 3)]
-                # is item in corresponding square?
-                if squareMap.get(item, None):
-                    return False
-                
-                row_hmap[item] = 1
-                cols[j][item] = 1
-                squareMap[item] = 1
-                
-                
+                quad[q].add(num)
+                row[i].add(num)
+                col[j].add(num)
         return True
+        
+        def dfs(i, j):
+            if (i >= ROWS):
+                return True
+            
+            if board[i][j] != ".":
+                newi, newj = i, j + 1 if j + 1 < COLS else i + 1, 0
+                return dfs(newi, newj)
+            
+            q = (i // 3, j // 3)
+            valid = False
+            for num in range(10):
+                if num not in quad[q] and num not in row[i] and num not in col[j]:
+                    quad[q].add(num)
+                    row[i].add(num)
+                    col[j].add(num)
+                    
+                    newi, newj = i, j + 1 if j + 1 < COLS else i + 1, 0
+                    if dfs(newi, newj):
+                        valid = True
+                    
+                    quad[q].remove(num)
+                    row[i].remove(num)
+                    col[j].remove(num)
+                    
+                    if valid:
+                        break
+            return valid
+                    
