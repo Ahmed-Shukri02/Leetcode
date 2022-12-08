1a643de0a9acdda80b2162dfdb910370dc391d3c
Ahmed Shukri <79210672+Ahmed-Shukri02@users.noreply.github.com>
2022-12-08 11:57:11 +0000

    Time: 0 ms (100.00%), Space: 6.2 MB (78.95%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.c
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.c
@@ -1,25 +1,53 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        lvs1, lvs2 = [], []
-        
-        def dfs(node, target):
-            if not node.left and not node.right:
-                target.append(node.val)
-                return
-            if node.left: dfs(node.left, target)
-            if node.right: dfs(node.right, target)
-        
-        
-        dfs(root1, lvs1)
-        dfs(root2, lvs2)
-        return lvs1 == lvs2
-            
-            
-            
-            
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int *arrP;
+
+bool leafSimilar(struct TreeNode* root1, struct TreeNode* root2){
+    int *lvs1 = (int*)malloc(200*sizeof(int));
+    int *lvs2 = (int*)malloc(200*sizeof(int));
+    
+    arrP = lvs1;
+    for(int i=0; i<200; i++){
+        arrP[i] = 0;
+    }
+    arrP = lvs1;
+    dfs(root1, &arrP);
+    
+    arrP = lvs2;
+    for(int i=0; i<200; i++){
+        arrP[i] = 0;
+    }
+    arrP = lvs2;
+    dfs(root2, &arrP);
+    
+    for(int i=0; i<200; i++){
+        if (lvs1[i] != lvs2[i]){
+            return false;
+        }
+    }
+    
+    return true;
+    
+}
+
+void dfs(struct TreeNode* nodeP, int** arr){
+    if(nodeP->left == NULL && nodeP->right == NULL){
+        **arr = nodeP->val;
+        ++*arr;
+        return;
+    }
+    
+    if(nodeP->left != NULL){
+        dfs(nodeP->left, arr);
+    }
+    if(nodeP->right != NULL){
+        dfs(nodeP->right, arr);
+    }
+}
