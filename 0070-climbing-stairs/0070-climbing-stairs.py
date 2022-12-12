53fd364c20d0050c44e471855695dff1cd995fff
Ahmed Shukri <79210672+Ahmed-Shukri02@users.noreply.github.com>
2022-12-12 14:55:36 +0000

    Time: 11 ms (98.35%), Space: 13.5 MB (12.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,8 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
-        
-        # bottom up dp
-        prev1, prev2 = 1, 2
-        
-        for i in range(n - 2):
-            ans = prev1 + prev2
-            prev1, prev2 = prev2, ans
-        
-        return prev2
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        n += 1
+        return int(round(((((1+(5**0.5))/2)**n)-(((1-(5**0.5))/2)**n))/(5**0.5)))
