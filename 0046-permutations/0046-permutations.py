f6d6a7939fac65351926ee9be7a8af2dbc8ed439
Ahmed Shukri <79210672+Ahmed-Shukri02@users.noreply.github.com>
2022-12-28 20:17:02 +0000

    Time: 25 ms (87.76%), Space: 13.6 MB (42.79%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        final = []
+        def helper(arr, curr):
+            if not arr:
+                final.append([i for i in curr])
+                return
+            for num in arr:
+                tmp = [i for i in arr]
+                curr.append(num)
+                tmp.remove(num)
+                helper(tmp, curr)
+                curr.pop()
+        helper(nums, [])
+        return final
+            
