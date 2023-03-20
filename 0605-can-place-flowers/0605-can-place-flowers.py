e405ba62ed3ed49205a45f9c00005e32827efc82
Ahmed Shukri <79210672+Ahmed-Shukri02@users.noreply.github.com>
2023-03-20 16:02:09 +0000

    Time: 194 ms (13.32%), Space: 14.5 MB (20.94%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,22 +1,11 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        mem = {} # mem[(i, k, prev)] = can I put k flowers from i onwards with a previous value of prev?
-
-        def dfs(i, k, prev):
-            if i >= len(flowerbed):
-                return k == 0
-            elif k == 0:
-                return True
-
-            if (i, k, prev) not in mem:
-                if prev == flowerbed[i] == flowerbed[min(i + 1, len(flowerbed) - 1)] == 0:
-                    flowerbed[i] = 1
-                    mem[(i, k, prev)] = dfs(i + 1, k - 1, 1)
-                else:
-                    mem[(i, k, prev)] = dfs(i + 1, k, flowerbed[i])
-
-            return mem[(i, k, prev)]
-
-        return dfs(0, n, 0)
-
+        for i in range(len(flowerbed)):
+            if flowerbed[max(i - 1, 0)] == flowerbed[i] == flowerbed[min(i + 1, len(flowerbed) - 1)] == 0:
+                flowerbed[i] = 1
+                n -= 1
+                i += 1
+            i += 1
+        print(n)
+        return n <= 0
             
